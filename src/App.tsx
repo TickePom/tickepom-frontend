aa6ee61223a1fde685cb3a886f31609fa3cb5728
Rubens M. Rodrigues <rubens_m.rodrigues@hotmail.com>
2023-02-02 13:10:49 -0300

    importação de fontes personalizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Paprika_400Regular } from '@expo-google-fonts/paprika';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Paprika_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Tickepom!</Text>
+      <Text style={{ fontFamily: 'Paprika_400Regular', fontSize: 30 }}>Tickepom</Text>
     </View>
   );
 }
